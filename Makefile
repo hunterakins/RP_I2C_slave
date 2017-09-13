f76ddfb63c45522e210088e34d8da33f1a670617
root <yunterakins@gmail.com>
2017-09-13 20:58:41 +0000

    fixed er upper

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CFLAGS  = -g -std=gnu99 -Wall -Werror
 CFLAGS += -I/opt/redpitaya/include
 CFLAGS += -I/home/redpitaya/RedPitaya/BitBanging
 LDFLAGS = -L/opt/redpitaya/lib
-LDLIBS = -lm -lpthread -lrp -lgsl -lgslcblas -lpthread
+LDLIBS = -lm -lpthread -lrp  -lpthread
 
 SRCS=$(wildcard *.c)
 OBJS=$(SRCS:.c=)
--- digitalOut.c
+++ digitalOut.c
@@ -4,7 +4,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <redpitaya/rp.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 int main() {
@@ -12,7 +11,7 @@ int main() {
 	if (rp_Init() < 0) {
 		printf("error with initialization\n");
 	}
-	
+
 	rp_dpin_t output;
 	output = RP_DIO1_P;
 	rp_DpinSetDirection(output, RP_OUT);
@@ -23,29 +22,6 @@ int main() {
 		rp_DpinSetState(output, RP_LOW);
 		usleep(100000);
 	}
-	//int i;
-	/*rp_pinState_t state;
-	state = RP_HIGH;
-	for (i = 0; i < 100; i++) {
-		fflush(stdout);		
-		usleep(3000000);
-		if (state == RP_HIGH) {
-			state = RP_LOW;
-			rp_DpinSetState(output, state);
-			printf("goinglow\n");
-		}
-		else {
-			state = RP_HIGH;
-			rp_DpinSetState(output, state);
-			printf("going high\n");
-		}	
-		
-	}
-	*/
-	if (RP_HIGH == 1) {
-		printf("we're in business\n");
-	}
-
 	rp_Release();
 
 	return 0;
